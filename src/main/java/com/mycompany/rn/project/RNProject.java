7d47f72f619ed81ad3a36bfdcf5570429bcd7505
karol marques <karoldm1579@gmail.com>
2023-01-02 22:33:13 -0300

    create ui interface and load csv files

---
+++ src/main/java/com/mycompany/rn/project/RNProject.java
@@ -0,0 +1,21 @@
+
+
+package com.mycompany.rn.project;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author karol
+ */
+public class RNProject {
+
+    public static void main(String[] args) {
+        JFrame form = new Form();
+        form.setName("Rede Neural");
+        form.setResizable(false);
+        form.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        form.setVisible(true);
+    }
+}
